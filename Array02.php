0ff87e84386615a6cc1c15cb0e34cf23c0780b4a
Xxyzarz7 <148178600+Xxyzarz7@users.noreply.github.com>
2024-01-09 09:14:24 +0700

    Add files via upload

---
+++ Array02.php
@@ -0,0 +1,27 @@
+<?php
+//contoh array
+//tugas 02
+$cars = array("Ferrary","Lamborgini", "audi");
+$negara = ["singapura","indonesia","jepang"];
+$kota = array("singapura","indonesia","jepang");
+
+// menampilkan array
+echo "I like " . $cars[0] . "," . $cars[1] . "and" . $cars[2] . ".";
+echo "<br>";
+var_dump($negara);
+// contoh pake{}
+echo"<br>";
+print_r($kota);
+// contoh pake ()
+
+//tugas03
+//menambahkan array
+// var_dump($kota);
+// $kota[] = "bogor";
+// echo "<br>";
+// var_dump($kota);
+// $kota[4] = "jayapura";
+// echo "<br>";
+// var_dump($kota);
+
+?>
